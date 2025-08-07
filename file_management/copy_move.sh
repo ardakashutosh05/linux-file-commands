db2aacef415e8a1a84c2466e1c96c6770569016a
Ashutosh Sudhirrao Ardak <ardakashutosh05@gmail.com>
2025-08-07 10:30:19 +0530

    Create copy_move.sh

---
+++ file_management/copy_move.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create a sample file
+echo "Sample content" > sample.txt
+
+# Copy the file
+cp sample.txt copy_sample.txt
+
+# Move the copied file
+mv copy_sample.txt moved_sample.txt
